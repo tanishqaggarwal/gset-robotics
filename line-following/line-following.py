148b93d7c41048b552364aaff3e7e8d111a60382
Tanishq Aggarwal <tanishq.aggarwal.11@gmail.com>
2016-07-02 13:32:06 -0400

    My partial line following

--- line-following/line-following.py
+++ line-following/line-following.py
@@ -3,10 +3,33 @@ from time import sleep
 print "running program"
 psm = PiStorms()
 
-psm.BAM1.setSpeed(75)
-psm.BBM1.setSpeed(75)
+exit = True
 
-sleep(5)
+WHITE = 600
+BLACK = 800
+TOLERANCE = 20
 
-psm.BAM1.float()
-psm.BBM1.float()
+psm.BAM1.setSpeedSync()
+
+previous_difference = 0
+direction = "LEFT" #Or "RIGHT"
+while (not exit):
+	light = psm.BAS1.lightSensorNXT(True)
+	current_difference = abs(light - BLACK)
+
+	if current_difference > TOLERANCE:
+		if previous_difference < current_difference:
+			
+
+
+	previous_difference = current_difference
+
+
+
+# psm.BAM1.setSpeed(75)
+# psm.BBM1.setSpeed(75)
+
+# sleep(5)
+
+# psm.BAM1.float()
+# psm.BBM1.float()
