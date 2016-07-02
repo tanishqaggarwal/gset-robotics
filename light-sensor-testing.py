3c837882886958c0e4c3c4119701a7b9a1049ed5
Tanishq Aggarwal <tanishq.aggarwal.11@gmail.com>
2016-07-02 11:58:29 -0400

    Added light sensor program

---
+++ light-sensor-testing.py
@@ -0,0 +1,14 @@
+from PiStorms import PiStorms
+from time import sleep
+print "running program"
+psm = PiStorms()
+
+exit = False
+
+while(not exit):
+    light = psm.BAS1.lightSensorNXT(True)
+    psm.screen.clearScreen()
+    psm.screen.termPrintln(str(light))
+
+    if (psm.isKeyPressed() == True):
+        exit = True
--- sample-motor-program.py
+++ sample-motor-program.py
@@ -3,10 +3,8 @@ from time import sleep
 print "running program"
 psm = PiStorms()
 
-psm.BAM1.setSpeed(75)
-psm.BBM1.setSpeed(75)
+psm.BAM1.setSpeedSync(75)
 
 sleep(5)
 
 psm.BAM1.float()
-psm.BBM1.float()
