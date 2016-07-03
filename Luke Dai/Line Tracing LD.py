76c1ca02b48d3b521c109896f9d2e0704dd8f109
hohohopopcorn <luke.s.dai@gmail.com>
2016-07-03 15:42:42 -0400

    Indents to Spaces

--- Luke Dai/Line Tracing LD.py
+++ Luke Dai/Line Tracing LD.py
@@ -17,21 +17,20 @@ largest=75-abs(initspeed)
 
 while(not exit):
     light=psm.BAS1.lightSensorNXT(True)
-	proportions=largest*(light-average)/width
-	psm.BAM1.setSpeed(initspeed-proportions)
-	psm.BAM2.setSpeed(initspeed+proportions)
+    proportions=largest*(light-average)/width
+    psm.BAM1.setSpeed(initspeed-proportions)
+    psm.BAM2.setSpeed(initspeed+proportions)
 
-	psm.led(1,255,0,255) 
-	sleep(0.1)
+    psm.led(1,255,0,255) 
+    sleep(0.1)
 
-	if(psm.isKeyPressed()): # if the GO button is pressed
-		psm.screen.clearScreen()
-		psm.screen.termPrintln("")
-		psm.screen.termPrintln("Exiting to menu")
-		psm.led(1,0,0,0)
+    if(psm.isKeyPressed()): # if the GO button is pressed
+        psm.screen.clearScreen()
+        psm.screen.termPrintln("")
+        psm.screen.termPrintln("Exiting to menu")
+        psm.led(1,0,0,0)
 
-		psm.BAM1.brake()
-		psm.BAM2.brake()
-		sleep(0.5)
-		
-		exit = True
+        psm.BAM1.brake()
+        psm.BAM2.brake()
+        sleep(0.5)
+        exit = True
