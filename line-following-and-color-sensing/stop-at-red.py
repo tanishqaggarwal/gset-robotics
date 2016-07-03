8f08d762900b9a1e36dcf3456fe44e597bbe9689
Tanishq Aggarwal <tanishq.aggarwal.11@gmail.com>
2016-07-03 17:56:42 -0400

    Edited stop at red

--- line-following-and-color-sensing/stop-at-red.py
+++ line-following-and-color-sensing/stop-at-red.py
@@ -9,8 +9,8 @@ psm = PiStorms()
 
 #exit variable will be used later to exit the program and return to PiStorms
 
-def linefollow(linefollowexit, q):
-    while(not linefollowexit):
+def linefollow():
+    while(not exit):
         light = psm.BAS1.lightSensorNXT(True)
         
         if (light <= 550):
@@ -23,46 +23,41 @@ def linefollow(linefollowexit, q):
         if (psm.isKeyPressed()):
             psm.BAM1.brake()
             psm.BAM2.brake()
-            linefollowexit = True
-        q.put(linefollowexit)
+            exit = True
 
     psm.BAM1.brake()
     psm.BAM2.brake()
     psm.BBM1.brake()
     psm.BBM2.brake()
 
-def findvicts(linefollowexit, victsexit, q):
-    while(not victsexit):
+def findvicts():
+    red = 8
+    red2 = 9
+    blue = 2
+
+    last_time = datetime.now()
+    while(not exit):
         color=hc.get_colornum()
-        if color==2:  #Blue
-            psm.led(1,255,0,0)
-            sleep(3)
+        if(color == red or color == red2):
+            exit = True
+        elif(color == blue and (datetime.now() - last_time).seconds > 2):
+            psm.led(1,0,0,255)
+            sleep(2)
             psm.led(1,0,0,0)
-            sleep(2)  #need to sleep long enough to not double count
-        elif color==9: #Red
-            linefollowexit = True
-            victsexit = True
-        else:
-            print color
         sleep(0.1)
         psm.led(1,0,0,0)  #Should not be needed but is safer
-        q.put(linefollowexit)
-        q.put(victsexit)
 
 hc=HiTechnicColorV2()
 psm.BBS1.activateCustomSensorI2C()
 
-victsexit = False
-linefollowexit = False
-q = Queue.Queue()
-lf = Thread(target=linefollow, args = (linefollowexit, q))
+lf = Thread(target=linefollow)
 lf.start()
-fv = Thread(target=findvicts, args = (linefollowexit, victsexit, q))
+fv = Thread(target=findvicts)
 fv.start()
 
 #Main Loop
 mainexit = False
-while(not mainexit):
+while(not exit):
     sleep(0.05)
 
     if(psm.isKeyPressed() == True): # if the GO button is pressed
@@ -75,4 +70,4 @@ while(not mainexit):
         psm.BBM1.brake()
         psm.BBM2.brake()    
         sleep(0.5)
-        mainexit = True
+        exit = True
