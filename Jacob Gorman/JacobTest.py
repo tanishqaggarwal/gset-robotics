976c4333e9035e071989bcfa17a876ca6ccaba14
Jacob Gorman <violinoman18@aol.com>
2016-07-03 16:26:18 -0400

    colors added turns modified

--- Jacob Gorman/JacobTest.py
+++ Jacob Gorman/JacobTest.py
@@ -3,19 +3,25 @@ from time import sleep
 print "running program"
 psm = PiStorms()
 
+blue = 2
+red = 8
 
 exit = False
 
 while(not exit):
     light = psm.BAS1.lightSensorNXT(True)
-    
-    if (light <= 550):
-        psm.BAM2.setSpeed(25)
-        psm.BAM1.setSpeed(-100)
+    color = psm.BBS1.colorSensorNXT(True)
+    if(light <= 550):
+        psm.BAM2.brake()
+        psm.BAM1.setSpeed(-50)
     else:
-        psm.BAM1.setSpeed(75)
-        psm.BAM2.setSpeed(-100)
-
+        psm.BAM1.setSpeed(25)
+        psm.BAM2.setSpeed(-50)
+    if(color == red):
+        psm.BAM2.brake()
+        psm.BAM1.brake()
+    elif(color == blue:
+        psm.screen.termprintln("blue")
     if (psm.isKeyPressed()):
         psm.BAM1.brake()
         psm.BAM2.brake()
