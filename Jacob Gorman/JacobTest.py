c01f2a3c6f5ca0dd798bd5e0006132d15aa89e4f
Jacob Gorman <violinoman18@aol.com>
2016-07-03 14:41:40 -0400

    negative

--- Jacob Gorman/JacobTest.py
+++ Jacob Gorman/JacobTest.py
@@ -11,10 +11,10 @@ while(not exit):
     
     if (light <= 550):
     	psm.BAM2.brake()
-    	psm.BAM1.setSpeed(50)
+    	psm.BAM1.setSpeed(-50)
     else:
     	psm.BAM1.brake()
-    	psm.BAM2.setSpeed(75)
+    	psm.BAM2.setSpeed(-75)
 
     if (psm.isKeyPressed()):
         exit = True
