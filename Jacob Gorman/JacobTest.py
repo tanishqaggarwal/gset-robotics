66d16dfcc4bea46bb93bde2398907cb9acbd8d6b
Jacob Gorman <violinoman18@aol.com>
2016-07-03 17:02:56 -0400

    Prrint

--- Jacob Gorman/JacobTest.py
+++ Jacob Gorman/JacobTest.py
@@ -8,7 +8,8 @@ psm = PiStorms()
 hc = HiTechnicColorV2()
 psm.BBS1.activateCustomSensorI2C()
 blue = 2
-red = 8
+red = 9
+red2 = 8
 
 exit = False
 
@@ -21,12 +22,12 @@ while(not exit):
     else:
         psm.BAM1.setSpeed(25)
         psm.BAM2.setSpeed(-50)
-    if(color == red):
+    if(color == red or color == red2):
         psm.BAM2.brake()
         psm.BAM1.brake()
     elif(color == blue):
         psm.screen.clearScreen()
-        psm.screen.termprintln("blue")
+        psm.screen.termPrintln("blue")
     if (psm.isKeyPressed()):
         psm.BAM1.brake()
         psm.BAM2.brake()
