eac0248451afd25b1e57dfe04eac52252f8622db
Jacob Gorman <violinoman18@aol.com>
2016-07-03 14:39:47 -0400

    put stuff into file

---
+++ Jacob Gorman/JacobTest.py
@@ -0,0 +1,20 @@
+from PiStorms import PiStorms
+from time import sleep
+print "running program"
+psm = PiStorms()
+
+
+exit = False
+
+while(not exit):
+    light = psm.BAS1.lightSensorNXT(True)
+    
+    if (light <= 550):
+    	psm.BAM2.brake()
+    	psm.BAM1.setSpeed(50)
+    else:
+    	psm.BAM1.brake()
+    	psm.BAM2.setSpeed(75)
+
+    if (psm.isKeyPressed()):
+        exit = True
