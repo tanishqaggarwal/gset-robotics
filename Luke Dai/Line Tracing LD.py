d75e865b0b2afa14f443ed9db4dad90d56aa6b11
hohohopopcorn <luke.s.dai@gmail.com>
2016-07-03 13:38:15 -0400

    Deleting Spaces

--- Luke Dai/Line Tracing LD.py
+++ Luke Dai/Line Tracing LD.py
@@ -17,19 +17,19 @@ MAX=100-initspeed
 
 while(not exit):
 	proportions=MAX*(psm.BAS1.lightSensorNXT(True)-average)/RANGE
-  	psm.BAM1.setSpeed(init+proportions)
- 	psm.BAM2.setSpeed(init-proportions)
+	psm.BAM1.setSpeed(init+proportions)
+	psm.BAM2.setSpeed(init-proportions)
 
 
 
 
-  psm.led(1,255,0,255) 
-  sleep(0.01)
+	psm.led(1,255,0,255) 
+	sleep(0.01)
 
-  if(psm.isKeyPressed() == True): # if the GO button is pressed
-    psm.screen.clearScreen()
-    psm.screen.termPrintln("")
-    psm.screen.termPrintln("Exiting to menu")
-    psm.led(1,0,0,0)
-    sleep(0.5)
-    exit = True
+	if(psm.isKeyPressed()==True): # if the GO button is pressed
+		psm.screen.clearScreen()
+		psm.screen.termPrintln("")
+		psm.screen.termPrintln("Exiting to menu")
+		psm.led(1,0,0,0)
+		sleep(0.5)
+		exit=True
