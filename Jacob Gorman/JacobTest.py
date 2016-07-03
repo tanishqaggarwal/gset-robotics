ea2ebea37789cd302f3fef981b3b43385e70c099
Jacob Gorman <violinoman18@aol.com>
2016-07-03 14:54:20 -0400

    Sharper turns

--- Jacob Gorman/JacobTest.py
+++ Jacob Gorman/JacobTest.py
@@ -10,11 +10,12 @@ while(not exit):
     light = psm.BAS1.lightSensorNXT(True)
     
     if (light <= 550):
-    	psm.BAM2.brake()
-    	psm.BAM1.setSpeed(-50)
+    	psm.BAM2.setSpeed(12)
+    	psm.BAM1.setSpeed(-25)
     else:
     	psm.BAM1.brake()
-    	psm.BAM2.setSpeed(-75)
+    	psm.BAM1.setSpeed(12)
+    	psm.BAM2.setSpeed(-25)
 
     if (psm.isKeyPressed()):
     	psm.BAM1.brake()
