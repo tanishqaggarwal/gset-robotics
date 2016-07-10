df8e501e6a03584b04bee0df8e04b1a79e9decc4
Tanishq Aggarwal <tanishq.aggarwal.11@gmail.com>
2016-07-10 16:22:41 -0400

    Added touch sensor capability

--- Jacob Gorman/JacobTest.py
+++ Jacob Gorman/JacobTest.py
@@ -32,6 +32,16 @@ def ninetydegrees(direction):
     else:
         s = 1
 
+def touch():
+    if psm.BAS2.isTouchedEV3() and psm.BAS1.isTouchedEV3():
+        return True
+    return False
+
+def masontouch():
+    if psm.BAS2.isTouchedEV3() or psm.BAS1.isTouchedEV3():
+        return True
+    return False
+
     psm.BAM1.setSpeed(s * 50)
     psm.BBM1.setSpeed(s * -50)
     sleep(0.285)
@@ -41,28 +51,42 @@ def ninetydegrees(direction):
 while(not exit):
     light = psm.BAS1.lightSensorNXT(True)
     color = hc.get_colornum()
-    if(light <= 550):
-        psm.BBM1.brake()
-        psm.BAM1.setSpeed(-50)
+
+    if masontouch():
+        #Do circumvention routine
+        move("backward")
+        sleep(0.5)
+        ninetydegrees("right")
+        move("forward")
+        sleep(1)
+        ninetydegrees("left")
+        move("forward")
+        sleep(0.5)
+        ninetydegrees("right")
     else:
-        psm.BAM1.setSpeed(25)
-        psm.BBM1.setSpeed(-50)
-    if(color == red or color == red2):
-        psm.BBM1.brake()
-        psm.BAM1.brake()
-        psm.screen.clearScreen()
-        psm.screen.termPrintln(str(counter))
-        exit = True
-    elif(color == blue):
-        if((datetime.now() - last_time).seconds > 0):
-            counter += 1
-            last_time = datetime.now()
+        #line follow, victim track
+        if(light <= 550):
+            psm.BBM1.brake()
+            psm.BAM1.setSpeed(-50)
+        else:
+            psm.BAM1.setSpeed(25)
+            psm.BBM1.setSpeed(-50)
+        if(color == red or color == red2):
+            psm.BBM1.brake()
+            psm.BAM1.brake()
             psm.screen.clearScreen()
-            psm.screen.termPrintln("I'm functioning Properly")
-    if (psm.isKeyPressed()):
-        psm.BAM1.brake()
-        psm.BBM1.brake()
-        exit = True
+            psm.screen.termPrintln(str(counter))
+            exit = True
+        elif(color == blue):
+            if((datetime.now() - last_time).seconds > 0):
+                counter += 1
+                last_time = datetime.now()
+                psm.screen.clearScreen()
+                psm.screen.termPrintln("I'm functioning Properly")
+        if (psm.isKeyPressed()):
+            psm.BAM1.brake()
+            psm.BBM1.brake()
+            exit = True
 
 psm.led(1, 255, 255, 255)
 sleep(1)
@@ -78,29 +102,31 @@ greenfound = False
 
 t = datetime.now()
 done = False
-while(not done and (datetime.now() - t).seconds < 5):
+while(not done):
     move("forward")
     color = hc.get_colornum()
-    if color == green:
+    if (color == green or if touch()):
         sleep(0.5)
         psm.BAM1.brake()
         psm.BBM1.brake()
         done = True
-        greenfound = True
+        if (color == green):
+            greenfound = True
         break
 
 if not greenfound:
     t = datetime.now()
     done = False
-    while(not done and (datetime.now()-t).seconds < 8):
+    while(not done):
         psm.BAM1.setSpeedSync(-50)
         color = hc.get_colornum()
-        if color == green:
+        if (color == green or touch()):
             sleep(0.5)
             psm.BAM1.brake()
             psm.BBM1.brake()
             done = True
-            greenfound = True
+            if (color == green):
+                greenfound = True
             break
 
     if not greenfound:
