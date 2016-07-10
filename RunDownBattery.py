f1edab0a4079044652d09baaf686b4299bd5857b
Tanishq Aggarwal <tanishq.aggarwal.11@gmail.com>
2016-07-10 17:20:14 +0000

    Improved turning accuracy

--- RunDownBattery.py
+++ RunDownBattery.py
@@ -3,14 +3,9 @@ from time import sleep
 from HiTechnicColorV2 import HiTechnicColorV2
 from datetime import datetime
 
-print "running program"
-
 psm = PiStorms()
 
-psm.BAM1.setSpeed(50)
-psm.BAM2.setSpeed(-50)
-
-sleep(0.2855)
-
+psm.BAM1.setSpeedSync(50)
+sleep(10)
 psm.BAM1.brake()
 psm.BAM2.brake()
